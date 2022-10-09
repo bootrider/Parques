82c174e83be8c14cc772e17182ab975ee8b55c80
Claudia Quiceno <claudiaeqr@gmail.com>
2022-10-08 21:30:45 -0500

    I need help for to complete funtion. Informatión is missing

--- DiceLogic.Tests/UnitTest1.cs
+++ DiceLogic.Tests/UnitTest1.cs
@@ -1,11 +1,68 @@
+
 namespace DiceLogic.Tests
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void roll_given1to6_returnTrueInterval() //se puede probar un intervalo?
         {
+            //arrange
+            var dice = new Dice(); //solo me permite probar la clase public no internal
+
+            //act
+            int number = dice.roll();
+
+            //assert
+            Assert.IsTrue(number > 0);
+            Assert.IsTrue(number < 7);
+        }
+
+        [TestMethod]
+        public void roll_lessTo1_returnFalse()
+        {
+            //arrange
+            var dice = new Dice(); //solo me permite probar la clase public no internal
+
+            //act
+            int number = dice.roll();
+
+            //assert
+            Assert.IsFalse(number < 1);
+        }
+
+        [TestMethod]
+        public void roll_moreTo6_returnFalse()
+        {
+            //arrange
+            var dice = new Dice(); //solo me permite probar la clase public no internal
+
+            //act
+            int number = dice.roll();
+
+            //assert
+            Assert.IsFalse(number > 6);
+        }
+
+
+        /*
+         * El codigo quiere probar el lanzamiento de dados, no de uno solo.
+         * Debería salir solo dos valores no 4 en el output de la prueba.
+         */
+        [TestMethod]
+        public void diceRoll_dices_returnX()
+        {
+            //arrange
+            var dice = new DiceLogicClass();
+
+            //act
+            var dado1 = dice.diceRoll();
+            var dado2 = dice.diceRoll();
+
+            //assert
+            //Assert.IsTrue(number > 0);
+            //Assert.IsTrue(number < 7);
+
         }
     }
 }
---
+++ DiceLogic/DiceLogicClass.cs
@@ -0,0 +1,16 @@
+﻿namespace DiceLogic
+{
+    public class DiceLogicClass
+    {
+        public object diceRoll() //lanzar dados
+        {
+            //no se especifico la firma del metodo, que retorna.
+            Dice dice1 = new Dice();
+            Dice dice2 = new Dice();
+            Console.WriteLine(dice1.roll()); 
+            Console.WriteLine(dice2.roll());
+            return null;
+        }
+        
+    }
+}
