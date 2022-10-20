af54a5df44c6473feeb737247c894a70aae7dadc
Claudia Quiceno <claudiaeqr@gmail.com>
2022-10-20 08:13:03 -0500

    first estructure of boardLogic, untested

---
+++ BoardLogic/BoardLogic.cs
@@ -0,0 +1,17 @@
+﻿namespace BoardLogic
+{
+    public class BoardLogic
+    {
+        int numberBoxSky = 8;
+        public Box[] board;
+        public BoxSky[][] ceil;
+        //public Token[][] carcel;
+
+        public BoardLogic(int numberPlayers)
+        { 
+            board = new Box[numberPlayers*16];
+            ceil = new BoxSky[numberPlayers][]; //no me reconoce el 8 al final
+
+        }
+    }
+}
