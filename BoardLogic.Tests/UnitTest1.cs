e32b1c0f43f1033c04c267b3890ea0ddf21f7099
Claudia Quiceno <claudiaeqr@gmail.com>
2022-11-08 11:40:55 -0500

    Changes for check

--- BoardLogic.Tests/UnitTest1.cs
+++ BoardLogic.Tests/UnitTest1.cs
@@ -6,12 +6,12 @@ namespace BoardLogic.Tests
         [TestMethod]
         public void Board_WithPlayers_ReturnTrue()
         {
-            int players = 3;
             //Arrange
+            int players = 3;
             var board = new Board(players);
 
             //Act
-            int cantHouse = board.Houses.Length;
+            int cantHouse = board.Houses.Length; //here debe to go a method for to test
 
             //Assert
             Assert.AreEqual(players, cantHouse);
