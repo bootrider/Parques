9baac4dbc1c1210369e1736dd8c0c0084d1f7054
Claudia Quiceno <claudiaeqr@gmail.com>
2022-11-09 22:49:33 -0500

    Add the BoxClass and the BoxTestClass.

---
+++ BoardLogic.Tests/BoxTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardLogic.Tests
+{
+    [TestClass]
+    public class BoxTests
+    {
+        [TestMethod]
+
+        public void GetTokensOverview_FirstColorsElementViolet_ReturTrue()
+        {
+            //Arrange
+            int indice = 2;
+            Box myBox = new Box(2);
+
+            Token token1 = new Token();
+            token1.Color = Color.Violet;
+
+            myBox.AddToken(token1);
+
+            //Act
+            var overview = myBox.GetTokensOverview();
+
+            //Assert
+            Assert.AreEqual(overview.First().Key, Color.Violet.Name);
+        }
+
+        [TestMethod]
+        public void RemoveAtoken_AddThreeTokensRemoveOne_ReturnTwoTokens()
+        {
+            //Arrange
+            Box myBox = new Box(5);
+
+            Token token1 = new Token();
+            token1.Color = Color.Violet;
+
+            Token token2 = new Token();
+            token2.Color = Color.White;
+
+            Token token3 = new Token();
+            token3.Color = Color.Red;
+
+            myBox.AddToken(token1);
+            myBox.AddToken(token2);
+            myBox.AddToken(token3);
+
+            //Act
+            myBox.RemoveAToken(token1);
+            var overview = myBox.GetTokensOverview();
+
+            //Assert
+            Assert.AreEqual(overview.Count,2);
+            Assert.AreEqual(overview.First().Key, Color.White.Name);
+            
+        }
+
+        [TestMethod]
+        public void RemoveAtoken_AddThreeTokensRemoveTwo_ReturnOneTokens()
+        {
+            //Arrange
+            Box myBox = new Box(5);
+
+            Token token1 = new Token();
+            token1.Color = Color.Violet;
+
+            Token token2 = new Token();
+            token2.Color = Color.Violet;
+
+            Token token3 = new Token();
+            token3.Color = Color.Red;
+
+            myBox.AddToken(token1);
+            myBox.AddToken(token2);
+            myBox.AddToken(token3);
+
+            //Act
+            myBox.RemoveAToken(token1);
+            myBox.RemoveAToken(token2);
+            var overview = myBox.GetTokensOverview();
+
+            //Assert
+            Assert.AreEqual(overview.Count, 1);
+            Assert.AreEqual(overview.First().Key, Color.Red.Name);
+        }
+
+        [TestMethod]
+        public void ClearBox_AddThreeTokensRemoveAll_isNull()
+        {
+            //Arrange
+            Box myBox = new Box(5);
+
+            Token token1 = new Token();
+            token1.Color = Color.Violet;
+
+            Token token2 = new Token();
+            token2.Color = Color.Violet;
+
+            Token token3 = new Token();
+            token3.Color = Color.Red;
+
+            myBox.AddToken(token1);
+            myBox.AddToken(token2);
+            myBox.AddToken(token3);
+
+            //Act
+            myBox.clearBox();
+            var overview = myBox.GetTokensOverview();
+
+            //Assert
+            Assert.AreEqual(overview.Count, 0);
+        
+        }
+
+    }
+}
--- BoardLogic/Box.cs
+++ BoardLogic/Box.cs
@@ -10,50 +10,65 @@ namespace BoardLogic
     public class Box
     {
         int indice = 0;
-        Dictionary<string, int> tokensOverview = new Dictionary<string, int>();    
+        //Dictionary<string, int> tokensOverview = new Dictionary<string, int>();
+        List<Token> myTokens = new List<Token>();
 
         public Box(int indice)
         {
             this.indice = indice;
         }
+
         public Dictionary<string, int> GetTokensOverview()
         {
-            return tokensOverview;
+            var myDict = new Dictionary<string, int>();
+            foreach (var token in this.myTokens)
+            {
+                if (myDict.ContainsKey(token.Color.Name))
+                {
+                    myDict[token.Color.Name] += 1;
+                }
+                else
+                {
+                    myDict.Add(token.Color.Name, 1);
+                }                
+            }
+            return myDict;
         }
 
         public void AddToken(Token token)
         {
-
-            if (tokensOverview.ContainsKey(token.Color.ToString()))
-            {
-                tokensOverview[token.Color.ToString()]+=1;
-            }
-            else 
-            {
-                tokensOverview.TryAdd(token.Color.ToString(), 1);
-            }        
+            this.myTokens.Add(token);
+            //if (tokensOverview.ContainsKey(token.Color.Name))
+            //{
+            //    tokensOverview[token.Color.Name]+=1;
+            //}
+            //else 
+            //{
+            //    tokensOverview.TryAdd(token.Color.Name, 1);
+            //}        
         }
 
         public void RemoveAToken(Token token)
         {
-            if (tokensOverview.TryGetValue(token.Color.ToString(), out int indice))
-            {
-                if (indice >= 2 )
-                {
-                    tokensOverview[token.Color.ToString()] -= 1;
-                }
-                else
-                {
-                    //id indice = 1, it is remove the color of dictionary
-                    tokensOverview.Remove(token.Color.ToString());    
-                }
-            }
+            this.myTokens.Remove(token);
+            //if (tokensOverview.TryGetValue(token.Color.ToString(), out int indice))
+            //{
+            //    if (indice >= 2 )
+            //    {
+            //        tokensOverview[token.Color.Name] -= 1;
+            //    }
+            //    else
+            //    {
+            //        //id indice = 1, it is remove the color of dictionary
+            //        tokensOverview.Remove(token.Color.Name);    
+            //    }
+            //}
             //si llega here at else, es por que, es por que el parametro no exite!!. exception
         }
 
         public void clearBox()
         {
-            tokensOverview.Clear();
+            this.myTokens.Clear();
         }
 
     }
